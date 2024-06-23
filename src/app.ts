848124bbdb29cfd1672d92ef82ff2d45f2173a14
Mohamed Lahcen <mohamed.lahcen@epitech.eu>
2024-06-23 14:05:29 +0200

    fix : app.js

--- src/app.ts
+++ src/app.ts
@@ -5,30 +5,20 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
 import { sequelize } from './config/sequelize';
-
 import userRoutes from './routes/userRoutes';
-import eventRoutes from './routes/eventRoutes';
-import eventDocumentRoutes from './routes/eventDocumentRoutes';
-import featuredEventRoutes from './routes/featuredEventRoutes';
 import ticketRoutes from './routes/ticketRoutes';
-import eventPackageRoutes from './routes/eventPackageRoutes';
-import packageItemRoutes from './routes/packageItemRoutes';
-import paymentRoutes from './routes/paymentRoutes';
-import subEventRoutes from './routes/subEventRoutes';
-import favoriteRoutes from './routes/favoriteRoutes';
 import userFollowRoutes from './routes/userFollowRoutes';
+import subEventRoutes from './routes/subEventRoutes';
+import paymentRoutes from './routes/paymentRoutes';
+import packageItemRoutes from './routes/packageItemRoutes';
+import featuredEventRoutes from './routes/featuredEventRoutes';
+import favoriteRoutes from './routes/favoriteRoutes';
+import eventRoutes from './routes/eventRoutes';
+import eventPackageRoutes from './routes/eventPackageRoutes';
+import eventDocumentRoutes from './routes/eventDocumentRoutes';
 
 const app = express();
 
-sequelize.sync({ force: true }).then(() => {
-  app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-  });
-}).catch(error => {
-  console.error('Unable to connect to the database:', error);
-});
-
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -40,16 +30,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/users', userRoutes);
-app.use('/events', eventRoutes);
-app.use('/event-documents', eventDocumentRoutes);
-app.use('/featured-events', featuredEventRoutes);
 app.use('/tickets', ticketRoutes);
-app.use('/packages', eventPackageRoutes);
-app.use('/package-items', packageItemRoutes);
-app.use('/payments', paymentRoutes);
-app.use('/sub-events', subEventRoutes);
-app.use('/favorites', favoriteRoutes);
 app.use('/user-follows', userFollowRoutes);
+app.use('/sub-events', subEventRoutes);
+app.use('/payments', paymentRoutes);
+app.use('/package-items', packageItemRoutes);
+app.use('/featured-events', featuredEventRoutes);
+app.use('/favorites', favoriteRoutes);
+app.use('/events', eventRoutes);
+app.use('/packages', eventPackageRoutes);
+app.use('/event-documents', eventDocumentRoutes);
 
 // catch 404 and forward to error handler
 app.use((req: Request, res: Response, next: NextFunction) => {
